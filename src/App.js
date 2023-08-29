df8e2d348cb3ad9840fff22ad6fed5cc5790b475
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-29 21:51:49 +0200

    Removed Wrapper

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { GlobalStyle } from "./GlobalStyled";
 
 import { AdviceGenerator } from "./Features/index";
-import { Wrapper } from "./styled";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Wrapper>
-        <AdviceGenerator />
-      </Wrapper>
+      <AdviceGenerator />
     </>
   );
 }
